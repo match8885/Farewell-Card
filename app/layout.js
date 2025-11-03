ef5155e88f969d882a21a668897c0bfd51c7096f
match8885 <match.tenkoh68@gmail.com>
2025-11-03 21:28:00 +0900

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <body className="bg-gradient-to-b from-sky-50 via-blue-50 to-blue-100 text-gray-800 overflow-x-hidden">
+      <body className="bg-gradient-to-b from-sky-50 via-blue-50 to-blue-100 text-gray-800 overflow-x-hidden font-zen">
         {children}
       </body>
     </html>
